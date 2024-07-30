5f5e6a40d559b9c88cbd86f1da5ffe902212e315
juyeonk <bpcoo99@gmail.com>
2024-07-30 12:07:35 +0900

    create vue project - blog

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Judy's Blog</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  
+  <div class="container mt-4">
+    <h5>쀼의 블로그</h5>
+    <p>vue로 만듦</p>
+  </div>
 
+  <List :blogs="blogs" />
+
+</template>
+   
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue';
+import blogData from './assets/data.js';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      blogs: blogData,
+    }
+  },
   components: {
-    HelloWorld
+    List: List,
   }
 }
 </script>
@@ -21,6 +58,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="blog in blogs" :key="blog.number">
+    <h5>{{ blog.title }}</h5>
+    <p>{{ blog.date }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'listComponent',
+  props: {
+    blogs: Array,
+  },
+}
+</script>
+
+<style>
+
+</style>
